af08705bffa7126701a33d056d0044a92a954a39
joaquimgomes8 <joaquimgomes2468@gmail.com>
2026-08-29 21:06:17 -0300

    desenvolvimento do projeto

--- index.py
+++ index.py
@@ -2,6 +2,13 @@ import tkinter as tk
 root = tk.Tk()
 root.title("AZShip System")
 root.geometry("400x300")
+root.attributes('-topmost',True)
+#===============================
+def bom_dia():
+    return print("Bom dia")
+#===============================
 
+button = tk.Button(root, text="Bom dia", command=FUNCAO)
 
+#===============================
 root.mainloop()
